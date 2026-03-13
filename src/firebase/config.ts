c8dcda9a17dc389cd928baf02e3ab9d55fabc8c2
Felipe Garcia <ed.felipe.gn@gmail.com>
2026-03-13 14:42:18 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-3240482316-e7d76",
+  "appId": "1:539534826625:web:e1f6ba2acf35de69328180",
+  "apiKey": "AIzaSyCr94GDrRYjk-kVFu73rKBX0kDC6nHAI8w",
+  "authDomain": "studio-3240482316-e7d76.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "539534826625"
+};
